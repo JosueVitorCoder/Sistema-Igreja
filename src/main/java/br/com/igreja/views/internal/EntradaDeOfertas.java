bab8c97f2fb9fcd04e8a7bf54b4c6eb66fe0e6f8
JosueVitorCoder <logiccoder88@gmail.com>
2024-06-03 08:12:52 -0300

    Commit de oferta através de internal view

--- src/main/java/br/com/igreja/views/internal/EntradaDeOfertas.java
+++ src/main/java/br/com/igreja/views/internal/EntradaDeOfertas.java
@@ -4,6 +4,14 @@
  */
 package br.com.igreja.views.internal;
 
+import br.com.igreja.models.Oferta;
+import br.com.igreja.models.dao.OfertaDAO;
+import br.com.igreja.util.JPAUtil;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.persistence.EntityManager;
+
 /**
  *
  * @author demi
@@ -29,9 +37,9 @@ public class EntradaDeOfertas extends javax.swing.JInternalFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jFormattedTextField1 = new javax.swing.JFormattedTextField();
-        jFormattedTextField2 = new javax.swing.JFormattedTextField();
-        jButton1 = new javax.swing.JButton();
+        formatTxtData = new javax.swing.JFormattedTextField();
+        formatTxtValor = new javax.swing.JFormattedTextField();
+        botao = new javax.swing.JButton();
 
         setClosable(true);
         setTitle("Entrada de Ofertas");
@@ -45,14 +53,19 @@ public class EntradaDeOfertas extends javax.swing.JInternalFrame {
         jLabel2.setText("Data: ");
 
         try {
-            jFormattedTextField1.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##/##/####")));
+            formatTxtData.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##/##/####")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
 
-        jFormattedTextField2.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter()));
+        formatTxtValor.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter()));
 
-        jButton1.setText("Salvar");
+        botao.setText("Salvar");
+        botao.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -64,15 +77,15 @@ public class EntradaDeOfertas extends javax.swing.JInternalFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addGap(18, 18, 18)
-                        .addComponent(jFormattedTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(formatTxtData, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jFormattedTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(formatTxtValor, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(113, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(botao)
                 .addGap(33, 33, 33))
         );
         jPanel1Layout.setVerticalGroup(
@@ -81,13 +94,13 @@ public class EntradaDeOfertas extends javax.swing.JInternalFrame {
                 .addGap(36, 36, 36)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jFormattedTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(formatTxtValor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jFormattedTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(formatTxtData, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jButton1)
+                .addComponent(botao)
                 .addContainerGap(30, Short.MAX_VALUE))
         );
 
@@ -108,13 +121,65 @@ public class EntradaDeOfertas extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void botaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoActionPerformed
+        cadastrarOferta();
+    }//GEN-LAST:event_botaoActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JFormattedTextField jFormattedTextField1;
-    private javax.swing.JFormattedTextField jFormattedTextField2;
+    private javax.swing.JButton botao;
+    private javax.swing.JFormattedTextField formatTxtData;
+    private javax.swing.JFormattedTextField formatTxtValor;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
+
+    private Oferta getOferta(){
+        double valor = getValor(formatTxtValor.getText());
+        Date data = getData();
+        Oferta oferta = new Oferta(valor, data);
+        return oferta;
+    }
+    
+    private double getValor(String text) {
+        String[] valorStr = text.split(",");
+        double valor = 0;
+        if(valorStr.length == 1){
+            valor = Double.parseDouble(text);
+        }else{
+            String valorS = text.replace(",", ".");
+            valor = Double.parseDouble(valorS);
+            }
+        return valor;
+    }
+    
+    private Date getData(){
+        String dataString = formatTxtData.getText();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date data = null;
+        try {
+          data = sdf.parse(dataString);
+          SimpleDateFormat formatoNovo = new SimpleDateFormat("yyyy-MM-dd");
+          String dataFormatada = formatoNovo.format(data);
+        } catch (ParseException e) {
+            System.out.println("Erro ao converter data: " + e.getMessage());
+        }
+        return data;  
+    }
+
+    private void cadastrarOferta() {
+        EntityManager em = JPAUtil.getEntityManager();
+        OfertaDAO dao = new OfertaDAO(em);
+        try{
+            em.getTransaction().begin();
+            dao.addOfertaAoBanco(getOferta());
+            em.getTransaction().commit();
+        }catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }finally{
+            em.close();
+        }
+    }
 }

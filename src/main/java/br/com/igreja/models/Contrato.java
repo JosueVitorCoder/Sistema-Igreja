280583de3f91c4309e7f158f41baf4145f5a1fca
JosueVitorCoder <logiccoder88@gmail.com>
2024-07-01 21:26:32 -0300

    Implementando modelo de contrato e iniciando contratoDAO

---
+++ src/main/java/br/com/igreja/models/Contrato.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.igreja.models;
+
+/**
+ *
+ * @author demi
+ */
+public class Contrato {
+    private Long id;
+    private String nomeLocatario;
+    private Integer vigenciaMeses;
+    private Double valorAluguel;
+
+    public Contrato(String nomeLocatario, Integer vigenciaMeses, Double valorAluguel) {
+        this.nomeLocatario = nomeLocatario;
+        this.vigenciaMeses = vigenciaMeses;
+        this.valorAluguel = valorAluguel;
+    }
+
+    public Contrato() {
+    }
+
+    public String getNomeLocatario() {
+        return nomeLocatario;
+    }
+
+    public Integer getVigenciaMeses() {
+        return vigenciaMeses;
+    }
+
+    public Double getValorAluguel() {
+        return valorAluguel;
+    }
+}
---
+++ src/main/java/br/com/igreja/models/dao/ContratoDAO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.igreja.models.dao;
+
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author demi
+ */
+public class ContratoDAO {
+    private EntityManager em;
+
+    public ContratoDAO(EntityManager em) {
+        this.em = em;
+    }
+    
+    
+}
